4310b23937aa50647c5a05552eb0fc95a3a36fde
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2022-08-12 15:12:45 +0300

    Create pythonprint.py

---
+++ .github/workflows/pythonprint.py
@@ -0,0 +1,9 @@
+'''
+EXERCICE: print the following
+One,'two',"three",four,\five\
+	once,'I' caught a fish
+		'//alive\\'
+'''
+print('One,\'two\',\"three\",four,\\five\\\n\tonce,\'I\' caught a\
+ fish\n\t\t\'//alive\\\\\'')
+
